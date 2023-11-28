217ef5af3ea2fd088b87e87fe6354f0eb03a2da6
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2023-11-28 13:04:02 +0330

    Start project
    + Some initial work was done ▼
    
    + Create a virtual environment (venv)
    
    + Install django>4.2,<4.3
    
    + Startproject and migrate (beyhagh_bastan)
    
    + Create a superuser (admin)
    
    + Startapp (main)

---
+++ beyhagh_bastan/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for beyhagh_bastan project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beyhagh_bastan.settings')
+
+application = get_asgi_application()
---
+++ beyhagh_bastan/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for beyhagh_bastan project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beyhagh_bastan.settings')
+
+application = get_wsgi_application()
