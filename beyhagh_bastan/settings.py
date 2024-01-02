34c62f36a293903d5ab8effd27b5636505b1d04e
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2024-01-02 11:59:19 +0330

    Site framework
    + Add site framework to site it is used for another functions

--- beyhagh_bastan/settings.py
+++ beyhagh_bastan/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites', # Added it for sites framework
     'django.contrib.humanize',
     'django_summernote',
     'jalali_date',
@@ -216,3 +217,5 @@ DECIMAL_SEPARATOR='.'
 
 NUMBER_GROUPING = 3
 # ***************************** Set Thousand Separator ********************************
+
+SITE_ID = 2 # Using site id for site framework
