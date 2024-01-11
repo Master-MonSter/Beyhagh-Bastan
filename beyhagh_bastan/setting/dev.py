e4d7e9a88077e07e121ca506250e3b1e93f69421
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2024-01-11 18:08:35 +0330

    Some security options
    + Add some security options in dev and prod

--- beyhagh_bastan/setting/dev.py
+++ beyhagh_bastan/setting/dev.py
@@ -5,7 +5,15 @@ from beyhagh_bastan.settings import *
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v8!i8204e5_v@_n82#d_2k@_92pn8=*5aqgvfu4-v3z0zr3hw@'
+from django.utils.crypto import get_random_string
+def get_random_secret_key():
+    """
+    Return a 50 character random string usable as a SECRET_KEY setting value.
+    """
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    return get_random_string(50, chars)
+SECRET_KEY = get_random_secret_key()
+# SECRET_KEY = 'django-insecure-v8!i8204e5_v@_n82#d_2k@_92pn8=*5aqgvfu4-v3z0zr3hw@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,15 +51,42 @@ EMAIL_HOST_PASSWORD ='byxy leux imad acea' # From beyhagh.bastan.antique@gmail.c
 # ******************************** reset password with email ********************************
 
 # ************************************** Django-compressor **************************************
-STATICFILES_FINDERS = (
-        # other finders..
-        'compressor.finders.CompressorFinder',
-)
-COMPRESS_ENABLED = True
-COMPRESS_CSS_FILTERS = ["compressor.filters.cssmin.CSSMinFilter"]
-COMPRESS_JS_FILTERS = ["compressor.filters.jsmin.JSMinFilter"]
-if not COMPRESS_ENABLED:
-       COMPRESS_ENABLED = True
-       COMPRESS_CSS_FILTERS = ["compressor.filters.cssmin.CSSMinFilter"]
-       COMPRESS_JS_FILTERS = ["compressor.filters.jsmin.JSMinFilter"]
+# STATICFILES_FINDERS = (
+#         # other finders..
+#         'compressor.finders.CompressorFinder',
+# )
+# COMPRESS_ENABLED = True
+# COMPRESS_CSS_FILTERS = ["compressor.filters.cssmin.CSSMinFilter"]
+# COMPRESS_JS_FILTERS = ["compressor.filters.jsmin.JSMinFilter"]
+# if not COMPRESS_ENABLED:
+#        COMPRESS_ENABLED = True
+#        COMPRESS_CSS_FILTERS = ["compressor.filters.cssmin.CSSMinFilter"]
+#        COMPRESS_JS_FILTERS = ["compressor.filters.jsmin.JSMinFilter"]
 # ************************************** Django-compressor **************************************
+
+# *************************************** Some secure settings *******************************************
+SECURE_HSTS_SECONDS = '60'  # 15780000  6 Months as Recommended
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+X_FRAME_OPTIONS = 'SAMEORIGIN'  # It's not be DENY because using summernote
+SECURE_CONTENT_TYPE_NOSNIFF = True
+# *************************************** Some secure settings *******************************************
+from ast import literal_eval
+from os import getenv
+# Feature flagged so can turn off in local development
+SECURE_SSL_REDIRECT = False
+# SECURE_SSL_REDIRECT = literal_eval(getenv("HTTPS_ONLY", "True"))
+# SECURE_SSL_REDIRECT = True 
+SESSION_COOKIE_SECURE = literal_eval(getenv("HTTPS_ONLY", "True"))
+# *************************************** Some secure settings *******************************************
+SECURE_BROWSER_XSS_FILTER = True
+## Strict-Transport-Security
+SECURE_HSTS_PRELOAD = True
+
+## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+
+# for more security
+CSRF_COOKIE_SECURE = True
+CSRF_USE_SESSIONS = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = 'Strict'
+# *************************************** Some secure settings *******************************************
